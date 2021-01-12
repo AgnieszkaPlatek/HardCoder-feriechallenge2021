4668adbd292c9169e9f6636deb0063896d974ce7
Agnieszka Platek <aga@platki.waw.pl>
2021-01-12 10:47:15 +0100

    Refactor

--- day7_password_generator.py
+++ day7_password_generator.py
@@ -32,19 +32,22 @@ def main():
             password = generate_strong_password(int(sys.argv[1]))
         else:
             password = generate_strong_password()
-    else:
-        n = input('Type the number of at least 8 characters in your password or press "t" to let it be 10.\n')
-        if n == 't':
-            password = generate_strong_password()
-        try:
-            n = int(n)
-            if n < 8:
-                print('It is not possible to generate a safe password with less than 8 characters, sorry!')
-                quit()
-            password = generate_strong_password(n)
-        except ValueError:
-            print('Please try again, make sure than the number consists of digits only.')
+        print('Your password is in the clipboard!')
+        pyperclip.copy(password)
+        quit()
+
+    n = input('Type the number of at least 8 characters in your password or press "t" to let it be 10.\n')
+    if n == 't':
+        password = generate_strong_password()
+    try:
+        n = int(n)
+        if n < 8:
+            print('It is not possible to generate a safe password with less than 8 characters, sorry!')
             quit()
+        password = generate_strong_password(n)
+    except ValueError:
+        print('Please try again, make sure than the number consists of digits only.')
+        quit()
 
     print('Your password is in the clipboard!')
     pyperclip.copy(password)
